c252482283770f38efd3113f623cc826fc05235b
Luis Angel Vargas Leon <LuisVargas@MacBook-LuisVargas.local>
2022-05-17 20:33:13 -0600

    login funcional

--- Login/src/Components/Login/Login.js
+++ Login/src/Components/Login/Login.js
@@ -12,6 +12,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { auth } from '../../shared/auth';
 
 
 function Copyright(props) {
@@ -30,13 +31,26 @@ function Copyright(props) {
 const theme = createTheme();
 
 const Login = () => {
+
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
+    let user = {
       email: data.get('email'),
       password: data.get('password'),
-    });
+    }
+    console.log(user);
+    const autorizacion =  auth(user);
+
+    console.log(autorizacion)
+
+    if(autorizacion){
+      alert('Bienvenido '+ autorizacion.name);
+    }
+    
+
   };
 
   return (
---
+++ Login/src/shared/users.js
@@ -0,0 +1,17 @@
+export const users = [
+    {
+        name: 'Carlos',
+        email: 'carlos@gmail.com',
+        password: '123456'
+    },
+    {
+        name: 'Pedro',
+        email: 'pedro@gmail.com',
+        password: '123abc'
+    },
+    {
+        name: 'Juan',
+        email: 'juan@gmail.com',
+        password: '321321'
+    },
+]
